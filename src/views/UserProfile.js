642fc5116e8cfabb28a2a9b9a4082a6424530ec6
Walter-Vanegas <walter.vanegas@coxautoinc.com>
2024-10-28 23:20:39 -0500

    add role touser

--- src/views/UserProfile.js
+++ src/views/UserProfile.js
@@ -20,6 +20,7 @@ function UserProfile() {
   const navigate = useNavigate ();
   const [user, setUser] = useState({});
   const [companies, setCompanies] = useState([]); 
+  const [roles, setRoles] = useState([]); 
 
   const onHandleChange = (e) => {
     const { name, value } = e.target;
@@ -38,9 +39,11 @@ function UserProfile() {
     const currentUserID = localStorage.getItem('currentUserID');
     const _companies = await axios.get(`${constants.apiurl}/api/companies`);
     setCompanies([{idcompany: -1, name: 'Sin Empresa'}, ..._companies.data]);
+    const _roles = await axios.get(`${constants.apiurl}/api/roles`);
+    setRoles([{idroles: -1, name: 'Sin Rol'}, ..._roles.data]);
     const _user =  await axios.get(`${constants.apiurl}/api/user/${currentUserID}`);
-    setUser(_user.data);
-   }
+      setUser(_user.data);
+    }    
    load();
   }, []);
 
@@ -62,7 +65,43 @@ function UserProfile() {
               <CardBody>
                 <Form>
                   <Row>
+                    <Col className="pr-md-1" md="4">
+                      <FormGroup>
+                        <label>Nombres</label>
+                        <Input placeholder="Jhon Doe" type="text" name='name' defaultValue={user.name} onChange={onHandleChange}/>
+                      </FormGroup>
+                    </Col>
+                    <Col className="pl-md-1" md="4">
+                      <FormGroup>
+                        <label>Numero de documento</label>
+                        <Input placeholder="00000000" type="number" name='dni' defaultValue={user.dni} onChange={onHandleChange}/>
+                      </FormGroup>
+                    </Col>
                     <Col md="4">
+                      <FormGroup>
+                        <label>Telefono</label>
+                        <Input placeholder="3000000000" type="tel" name='phone' defaultValue={user.phone} onChange={onHandleChange} />
+                      </FormGroup>
+                    </Col>
+                  </Row>
+                  <Row>                    
+                    <Col md="3">
+                      <FormGroup>
+                        <label htmlFor="exampleInputEmail1">
+                          Email
+                        </label>
+                        <Input placeholder="jhondoe@email.com" type="email" autoComplete="off" name='email' defaultValue={user.email} onChange={onHandleChange} />
+                      </FormGroup>
+                    </Col>
+                    <Col md="3">
+                      <FormGroup>
+                        <label htmlFor="exampleInputEmail1">
+                          Contraseña
+                        </label>
+                        <Input placeholder="*****" type="password" autoComplete="off" name='password' defaultValue={user.password} onChange={onHandleChange}/>
+                      </FormGroup>
+                    </Col>
+                    <Col md="3">
                       <FormGroup>
                         <label>Empresa</label>
                         <select className="form-control" name="idCompany" value={user.idCompany} onChange={cmbCompanyOnChange}>
@@ -73,45 +112,18 @@ function UserProfile() {
                         </select>
                       </FormGroup>
                     </Col>
-                    <Col md="4">
+                    <Col md="3">
                       <FormGroup>
-                        <label htmlFor="exampleInputEmail1">
-                          Email
-                        </label>
-                        <Input placeholder="jhondoe@email.com" type="email" autoComplete="off" name='email' defaultValue={user.email} onChange={onHandleChange} />
+                        <label>Rol</label>
+                        <select className="form-control" name="idRol" value={user.idRol} onChange={onHandleChange}>
+                        {
+                            roles?.map((rol, index) => 
+                            <option key={index} value={rol.idroles}>{rol.name}</option>
+                        )} 
+                        </select>
                       </FormGroup>
                     </Col>
-                    <Col md="4">
-                      <FormGroup>
-                        <label htmlFor="exampleInputEmail1">
-                          Contraseña
-                        </label>
-                        <Input placeholder="*****" type="password" autoComplete="off" name='password' defaultValue={user.password} onChange={onHandleChange}/>
-                      </FormGroup>
-                    </Col>
-                  </Row>
-                  <Row>
-                    <Col className="pr-md-1" md="6">
-                      <FormGroup>
-                        <label>Nombres</label>
-                        <Input placeholder="Jhon Doe" type="text" name='name' defaultValue={user.name} onChange={onHandleChange}/>
-                      </FormGroup>
-                    </Col>
-                    <Col className="pl-md-1" md="6">
-                      <FormGroup>
-                        <label>Numero de documento</label>
-                        <Input placeholder="00000000" type="number" name='dni' defaultValue={user.dni} onChange={onHandleChange}/>
-                      </FormGroup>
-                    </Col>
-                  </Row>
-                  <Row>
-                    <Col md="4">
-                      <FormGroup>
-                        <label>Telefono</label>
-                        <Input placeholder="3000000000" type="tel" name='phone' defaultValue={user.phone} onChange={onHandleChange} />
-                      </FormGroup>
-                    </Col>
-                  </Row>
+                  </Row>                 
                 </Form>
               </CardBody>
               <CardFooter>
